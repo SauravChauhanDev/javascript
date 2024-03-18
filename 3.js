63b4e98f8dd1335dd9ff86e0da9a4f8a2d4c7f50
SauravChauhanDev <sauravchauhan.self@gmail.com>
2024-03-19 01:25:25 +0530

    01:29:46  -  comparison of datatypes

---
+++ 3.js
@@ -0,0 +1,24 @@
+"use strict" //treat all js code as newer verion
+
+
+//alert("Hello"); //we are using node js not browser
+
+
+//Datatype
+
+//string
+//bigInt
+//string => ""
+//boolean => true/false
+//null => Standalone value
+//undefine => not assign value 
+//symbol => unique
+
+//object
+//
+
+console.log(typeof("Saurav"));
+console.log(typeof(null));
+console.log(typeof(undefined));
+console.log(typeof(1234));
+console.log(typeof(bigInt));
---
+++ 4.js
@@ -0,0 +1,20 @@
+// Datatype conversion Confusion
+// Operation
+
+let age = "19abc";
+let score = 33;
+
+console.log(typeof(score)); // As a method
+console.log(typeof score);
+console.log(typeof(age));
+let changeNumber = Number(age)
+console.log(typeof(changeNumber), changeNumber);
+
+//Nan => not a number if you can change the string into numer so its shaw Nan
+
+"33" = 33
+"Saurav" = NaN
+true = 1
+flase = 0 
+null = 0
+undefine = Nan
