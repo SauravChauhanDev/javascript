a7f130cc0c9c164b0ecc4b97ff32e7ad88618dba
SauravChauhanDev <sauravchauhan.self@gmail.com>
2024-03-19 00:51:56 +0530

    2.let Const Var Scope and Comment

--- 1.js
+++ 1.js
@@ -1,3 +1,4 @@
-console.log("Hello World");
-console.log('Hello World');
-console.log(`Hello World`);
+//Different Way to Print Anything
+console.log("Hello World"); //Using ""
+console.log('Hello World'); //using ''
+console.log(`Hello World`); //using `` 
---
+++ 2.js
@@ -0,0 +1,28 @@
+// Let Const Var
+// For Storing the data
+// Const: you can't change it
+
+// how to Declare the Const let and var
+const accountId = 123455;
+let    = "sauravchauhan.manage@gmail.com";
+var accountPassword = "12dfgsf345";
+accounttype = "Saving";
+
+accountEmail = "Saurav";
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accounttype);
+
+console.table([accountId, accountEmail, accountPassword, accounttype])
+
+// { } Scope 
+
+/*
+    COmment
+*/
+
+/*
+    Prefer  not to use var
+    because of issue in block scope and function scope
+*/
